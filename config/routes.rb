46b7cb13b65b60a74a3566986d2846ab1ab24f9a
Logan Medina <loganjmedina@gmail.com>
2017-04-05 16:15:24 -0700

    added registrations override to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations'}
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   root to: "splash#index"
