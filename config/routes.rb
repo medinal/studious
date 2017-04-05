cc4ed0f2abc995741a89545145396b846ad74e98
Logan Medina <loganjmedina@gmail.com>
2017-04-05 16:51:22 -0700

    changed splash to home

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  root to: "splash#index"
+  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
