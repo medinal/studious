b304a5532802211cbed90b6933997aea70b947b3
Logan Medina <loganjmedina@gmail.com>
2017-04-05 16:12:11 -0700

    when a user is saved their 'friendly' id token is generated

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+
+  def create_token
+    begin
+     self.token = SecureRandom.hex[0,10].upcase
+     rescue ActiveRecord::RecordNotUnique
+       retry
+    end
+  end
+  
 end
