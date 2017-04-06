f5b6c885458eee4e2606811eb7b47c87196b5acd
Logan Medina <loganjmedina@gmail.com>
2017-04-06 09:54:56 -0700

    user model testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,7 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Warden::Test::Helpers
 
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
