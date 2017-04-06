6a4baa6101a1aae0644a7844e6123f4dc65a46b1
Logan Medina <loganjmedina@gmail.com>
2017-04-06 09:38:09 -0700

    set up user model for testing and configured the helpers to use devise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,47 @@
-require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(name: "name", email: "example@example.com", password: "123456")}
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without e-mail" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a string without an @ symbol" do
+    subject.email = "test"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a string without an ." do
+    subject.email = "test@test"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a password" do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a short password" do
+    subject.password = "25"
+    expect(subject).to_not be_valid
+  end
+
+  it "has a default role of student" do
+    expect(subject.role).to eq("student")
+  end
+
+  it "creates a unique identifier" do
+    subject.create_identifier
+    expect(subject.identifier).to_not be_nil
+  end
+
 end
