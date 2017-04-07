926bb6650a723ae572e06853aa74f12f4b92cac1
Logan Medina <loganjmedina@gmail.com>
2017-04-06 20:20:55 -0700

    reordered routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {registrations: 'user/registrations'}
-
+  root to: "home#index"
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  root to: "home#index"
-
-  resources :users, only: [:index] do
-    resource :profile, only: [:show]
-    resources :projects, only: [:index, :show]
-  end
+  devise_for :users, controllers: {registrations: 'user/registrations'}
 
   resource :student, only: [:show] do
     resource :profile
     resources :projects
   end
 
+  resources :users, only: [:index] do
+    resource :profile, only: [:show]
+    resources :projects, only: [:index, :show]
+  end
+  
 end
