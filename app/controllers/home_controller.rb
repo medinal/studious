b699d8583d0c86f2c9858e8a86747244afcc8c79
Logan Medina <loganjmedina@gmail.com>
2017-04-07 17:37:07 -0700

    fixed some styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
 
+  before_action :disable_sidebar, only: [:index]
+
   def index
+  end
+
+  private
+
+  def disable_sidebar
+    @disable_sidebar = true
   end
 
 end
