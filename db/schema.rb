991bb1c614bc9b71ef0fa9ff5ef9aa1c4458de25
Logan Medina <loganjmedina@gmail.com>
2017-04-11 16:14:01 -0700

    added video and link urls to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170409231812) do
+ActiveRecord::Schema.define(version: 20170411231313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,8 @@ ActiveRecord::Schema.define(version: 20170409231812) do
     t.integer  "user_id"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.string   "video_url"
+    t.string   "link_url"
     t.index ["user_id"], name: "index_projects_on_user_id", using: :btree
   end
 
